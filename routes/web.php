71323541cb33820608849197c9e989bb5a43ef17
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-02-13 16:05:46 +0200

    Integrate HTML Template in fontend and splitting page

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/' , [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
