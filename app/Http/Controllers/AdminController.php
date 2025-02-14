94fc3328578e87e7a668be8e56dabd5cd40b0943
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-02-14 19:50:09 +0200

    Show Toastr Message after add cagegory

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 
+
 class AdminController extends Controller
 {
+    //to show category page
     public function view_category(){
 
         return view('admin.category');
     }
 
+    //to add category to database
     public function add_category(Request $request){
 
           $category = new Category;
@@ -19,6 +22,9 @@ class AdminController extends Controller
          $category->category_name = $request->category;
 
          $category->save();
+
+          toastr()->closeButton()->addSuccess('category added successfully');
+
 
          return redirect()->back();
 
