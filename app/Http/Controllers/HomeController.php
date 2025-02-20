e7d8032f6de539cfec5d15a74a687f8939f657c1
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-02-20 17:37:43 +0200

    add user make order table and place order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\User;
 use App\Models\Cart;
+use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -21,19 +22,18 @@ class HomeController extends Controller
 
     public function home()
     {
-        
+
 
         $product = Product::all();
-        if(Auth::id()){
+        if (Auth::id()) {
             $user = Auth::user();
             $userId = $user->id;
-            $count = Cart::Where('user_id' , $userId)->count();
-        }
-        else{
+            $count = Cart::Where('user_id', $userId)->count();
+        } else {
             $count = '';
         }
-     
-        return view('home.index', compact('product' , 'count'));
+
+        return view('home.index', compact('product', 'count'));
     }
 
     public function login_home()
@@ -41,34 +41,33 @@ class HomeController extends Controller
 
 
         $product = Product::all();
-        if(Auth::id()){
+        if (Auth::id()) {
             $user = Auth::user();
             $userId = $user->id;
-            $count = Cart::Where('user_id' , $userId)->count();
-        }
-        else{
+            $count = Cart::Where('user_id', $userId)->count();
+        } else {
             $count = '';
         };
-        return view('home.index', compact('product'  , 'count'));
+        return view('home.index', compact('product', 'count'));
     }
 
     public function product_details($id)
     {
         $data = Product::find($id);
-        if(Auth::id()){
+        if (Auth::id()) {
             $user = Auth::user();
             $userId = $user->id;
-            $count = Cart::Where('user_id' , $userId)->count();
-        }
-        else{
+            $count = Cart::Where('user_id', $userId)->count();
+        } else {
             $count = '';
         }
-        return view('home.product_details', compact('data' , 'count'));
+        return view('home.product_details', compact('data', 'count'));
     }
 
 
     // add to cart if user login 
-    public function add_cart($id){
+    public function add_cart($id)
+    {
         $product_id = $id;
         $user = Auth::user();
         $user_id = $user->id;
@@ -82,21 +81,50 @@ class HomeController extends Controller
     }
 
 
-    public function mycart(){
-        if(Auth::id()){
+    public function mycart()
+    {
+        if (Auth::id()) {
             $user = Auth::user();
             $userId = $user->id;
-            $count = Cart::Where('user_id' , $userId)->count();
-            $cart = Cart::where('user_id' , $userId)->get();
+            $count = Cart::Where('user_id', $userId)->count();
+            $cart = Cart::where('user_id', $userId)->get();
         }
-      
-        return view('home.mycart' ,compact('count' , 'cart'));
+
+        return view('home.mycart', compact('count', 'cart'));
     }
 
-    public function delete_cart($id){
+    public function delete_cart($id)
+    {
 
         $delete_cart =  Cart::find($id);
         $delete_cart->delete();
         return redirect()->back();
+    }
+
+    public function confirm_order(Request $request)
+    {
+
+        $name = $request->name;
+        $address = $request->address;
+        $phone = $request->phone;
+        $userid = Auth::user()->id;
+        $cart = Cart::where('user_id', $userid)->get();
+        foreach ($cart as $carts) {
+            $order = new Order;
+            $order->name = $name;
+            $order->rec_address = $address;
+            $order->phone = $phone;
+            $order->user_id = $userid;
+            $order->product_id = $carts->product_id;
+            $order->save();
+        }
+        // $cart_remove = Cart::where('user_id' ,$userid)->get();
+        // foreach($cart_remove as $remove)
+        // {
+        //     $data  = Cart::find($remove->id);
+        //     $data->delete();
+        // }
+        return redirect()->back();
+
     }
 }
--- resources/views/home/mycart.blade.php
+++ resources/views/home/mycart.blade.php
@@ -31,11 +31,27 @@
 
             border: 1px solid skyblue;
         }
-        .cart_vlaue{
+
+        .cart_vlaue {
 
             text-align: center;
             margin-bottom: 50px;
             padding: 50px;
+        }
+
+        .order_deg {
+
+            padding-right: 100px;
+            margin-top: -50px;
+
+        }
+        label{
+
+            display: inline-block;
+            width: 150px;
+        }
+        .div_gap{
+            padding: 20px;
         }
     </style>
 </head>
@@ -48,6 +64,27 @@
 
 
         <div class="dev_deg">
+            <div class="order_deg">
+                <form action="{{ url('confirm_order') }}" method="POST">
+                    @csrf
+
+                    <div class="div_gap">
+                        <label for="">Receiver Name</label>
+                        <input type="text" name="name" value="{{ Auth::user()->name }}">
+                    </div>
+                    <div class="div_gap">
+                        <label for="">Receiver Address</label>
+                        <textarea name="address" >{{ Auth::user()->address }}</textarea>
+                    </div>
+                    <div class="div_gap">
+                        <label for="">Receiver Phone</label>
+                        <input type="text" name="phone" value="{{ Auth::user()->phone }}"> 
+                    </div>
+                    <div class="div_gap">
+                        <input class="btn btn-primary" type="submit" value="place Order">
+                    </div>
+                </form>
+            </div>
             <table>
                 <tr>
                     <th>Title</th>
@@ -56,30 +93,28 @@
                     <th>Action</th>
                 </tr>
                 <?php
-                $value = 0 ;
-
-
+                $value = 0;
+                
                 ?>
                 @foreach ($cart as $cart)
                     <tr>
                         <td>{{ $cart->product->title }}</td>
                         <td>{{ $cart->product->price }}</td>
                         <td><img width="150" src="/products/{{ $cart->product->image }}"></td>
-                        <td><a class="btn btn-danger" href="{{ url('delete_cart' , $cart->id) }}">Remove</a></td>
+                        <td><a class="btn btn-danger" href="{{ url('delete_cart', $cart->id) }}">Remove</a></td>
                     </tr>
                 @endforeach
 
 
                 <?php
-                $value =  $value +  $cart->product->price;
-
-
+                $value = $value + $cart->product->price;
+                
                 ?>
             </table>
 
         </div>
         <div class="cart_vlaue">
-            <h3>Total Value of cart is: {{ $value  }}LYD </h3>
+            <h3>Total Value of cart is: {{ $value }}LYD </h3>
         </div>
 
 
